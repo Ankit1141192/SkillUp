1e1d6a4c9a4d4e4ba24a2362bd9f07a333b7781a
Ankit Kumar <ankit2914978@gmail.com>
2025-06-25 23:43:35 +0530

    Initial commit
    Generated by react-native@0.80.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>SkillUp E-learning App</Text>
+    </View>
+  )
+}
+
+export default App
